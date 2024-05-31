4d102641c8010c7bad5d1322a703488e95729e22
jilmerroca <jilmer.roca@gmail.com>
2024-05-31 05:20:36 -0500

    Add Spring Security to pom

---
+++ src/main/java/com/codigo/apigateway/util/JwtUtil.java
@@ -0,0 +1,25 @@
+package com.codigo.apigateway.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    @Value("${key.token}")
+    private String keytoken;
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(keytoken);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
